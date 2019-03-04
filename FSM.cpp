08c0ecc7748a881f4bac813c34222bbf9e57a009
Maria Diaz <adiaz064@gmail.com>
2019-03-03 16:20:08 -0800

    Update FSM.cpp

--- FSM.cpp
+++ FSM.cpp
@@ -1,67 +1,105 @@
-#include "FSM.h"
+#include <iostream>
 #include <string>
+#include <cctype>
+#include "FSM.h"
 #include "lexer.h"
 
-using namespace std;
-
-
-
-FSM::FSM()
-{
+FSM::FSM() {
+  //contructor
+  FSMtable[6][7] = {
+      1, 2, 4, 0, 0, 0, 0,
+      1, 1, 4, 0, 0, 0, 0, 
+      5, 2, 4, 0, 3, 0, 0, 
+      4, 4, 0, 4, 4, 4, 4, 
+      0, 0, 0, 0, 0, 0, 0
+    };
 }
 
+/*------------------Sammi's state trans table----------------------
+---------------------Credit Given (Period 1)-----------------------
+This Function is designed to navigate through the State Transition table.
+Given the specific character being currently read from the file, the state will
+change according to the table. Table is initialized in global scope but can be
+referenced as such:
+				 char num  '!' sep '.' ops  '\n'
+	new(0)			1,	2,	4,	0,	0,	0,  0
+	string(1)		1,	1,	4,	0,	0,	0,  0
+	int(2)			5,	2,	4,	0,	3,	0,  0
+	float(3)		5,	3,	4,	0,	5,	0,  0
+	comment(4)	4,	4,	0,	4,	4,	4,  4
+	invalid(5)	0,	0,	0,	0,	0,	0   0
+*/
 
-enum class FSM::states {
-	STR, INT, FLOAT, COM, NEW, INVALID
-};
+int FSM::findState(char temp, int state) {
+  lexer lexerObject;
+  int currentState = state;
+  if(isalpha(temp)) {//if temp is alphabetic
+    currentState = FSMtable[currentState][0];
+    return currentState;
+  } else if(isdigit(temp) || temp == '$') {
+    //if it's a number or dollar sign
+    currentState = FSMtable[currentState][1];
+    return currentState;
+  } else if(temp == lexerObject.operators[19]) {
+    //[20] == '!'
+    currentState = FSMtable[currentState][2];
+    return currentState;
+  } else if(temp == lexerObject.operators[15]) {
+    //[16] == '.'
+    currentState = FSMtable[currentState][4];
+    return currentState; 
+  } else if(isspace(temp)) {
+    currentState = FSMtable[currentState][6];
+  } else {
 
-FSM::FSM(int states, int initialState, int acceptingStates, int nextState) {
+    for(int i = 0; i < 8; i++) {
+      if(temp == lexerObject.operators[i] || isspace(temp)) {
+        currentState = FSMtable[currentState][5];
+        return currentState;
+      }
+    }
 
-
+    for(int i = 8; i < 22; i++) {
+      if(temp == lexerObject.operators[i] || isspace(temp)) {
+        currentState = FSMtable[currentState][3];
+        return currentState;
+      }
+    }
+  }
 }
 
-int nextState(char currentState, char input) {
-	//I believe that each return will be an enumeration
-	switch (currentState) {
-	case 1:
-		if (input == 'a' || input == 'b') {
-		return 1;
-		}
-		break;
+void FSM::stateTransition(int currentState, char ch, string temp) {
+  lexer lexerObject;
+  if(currentState != 4) {
+    currentState = findState(currentState, ch);
+    switch(currentState) {
+      case 0:
+        lexerObject.testLexeme(temp);
+        lexerObject.sepOrOp(ch);
+        break;
+      case 1 || 2 || 3:
+        temp += ch;
+        break;
+      case 4:
+        lexerObject.testLexeme(temp);
+        temp = "";
+        break;
+      case 5:
+        lexerObject.testLexeme(temp);
+        lexerObject.sepOrOp(ch);
+        temp = "";
+        }
+      }
+    else if(ch == '!' && currentState == 4) {
+      currentState = FSMtable[currentState][2];
+    }
 
-	case 2:
-		if (input == 'c') {
-			return 3;
-		}
-		break;
-
-	default:
-		break;
-}
-
-return NoNextState;//constant to specify that there is no next state
-	
-}
-
-void FSM::run(char input) {
-
-}
-
-void FSM::run(char input, string file) {
-	//runs this FSM on the specified 'input' string
-	//returns 'true' if 'input' or a subset of 'input' matches
-	//the regular experession corresponding to this FSM
-
-	int currentState = initialState;
-
-	for (int i = 0; i < input.length(); i++) {
-		//Code to get a character
-		readFile(file);
-	}
+    lexerObject.testLexeme(temp);
+    temp = "";
+  }
 
 
-}
-
-FSM::~FSM()
-{
-}
+//displayTemp(temp);
+  //showTokenLexeme(string, string);
+//displayChar(ch);
+  //sepOrOp(ch);
