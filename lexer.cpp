199ad10824fdf6f7243ad4b541dfd93ffc00c876
Maria Diaz <adiaz064@gmail.com>
2019-03-03 16:21:16 -0800

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -1,31 +1,89 @@
-#include <fstream>
 #include <iostream>
-#include "lexer.h"
+#include <string>
+#include <vector>
+#include <cctype>
 #include "FSM.h"
+#include "lexer.h"
 
 
-using namespace std; 
+lexer::lexer() {
+  char operators[20] = {
 
-void lexer:: readFile(string file)
-{
-	fstream inFile;
-	char temp;
-	inFile.open(file.c_str(), ios::in | ios::out);
-	int state = 0; 
+      ' ',',',';','.',':',
+      '[',']','{','(',')', 
+      '}','/','\'','%','>',
+      '<','=','-','+','*' 
 
-	while (inFile >> temp >> noskipws)
-	{
-		finiteStateMachine(state, temp);
+    };
 
-	}
-	//cin.get();
+    string keywords[11] = {
+      "if", "else", "then",
+      "while", "do", "for", 
+      "and", "or", "bool", 
+      "float", "int"
+    };
 }
 
+//We might have to make it so this
+//takes the vector of lexemes
+void lexer::testLexeme(string temp) {
+  //called for separator, keyword, or invalid
+  //tests input for : integer, float, identifier, keyworkd, invalid
+  if(temp != "") {
+    //instantiate your boolean
+    bool isKeyword = 0;
+    for(int i = 0; i < 11; i++) {//are there more keywords
+                                //that we should include?
+      if(keywords[i] == temp) {
+        //show the output for a keyword
+        showTokenLexeme.push_back(pair<string, string>("Keyword: \t", temp));
+        return;
+      }
+    }
+    isKeyword = 1;//(TRUE)
+    if(isalpha(temp[0]) && isKeyword == 1) {
+      //then it's an identifier, show output for that
+      showTokenLexeme.push_back(pair<string, string>("Identifier: \t", temp));
+      return;
+    } 
 
-//The code below should be FSM.run()
-void lexer::finiteStateMachine(int state, char a)
-{
+    int isFloat = 0;
+    for(int i = 0; i < temp.length(); i++) {
+      if(temp[i] == '.') {
+        isFloat = 1;
+        break;//it's a float. Stop the whole while loop here
+      }
+    }
+    if(isFloat == 0) {
+      showTokenLexeme.push_back(pair<string, string>("Integer: \t", temp));
+      return;
 
+    } else if(isFloat == 1) {
+      showTokenLexeme.push_back(pair<string, string>("Float: \t", temp));
+      return;
+    }
+
+    //Lastly, it must be invalid
+    showTokenLexeme.push_back(pair<string, string>("Invalid: \t", temp));
+    return;
+  }
+  
+}
+
+void lexer::sepOrOp(char ch) {
+  //operate w/ strings of 
+  //single byte characters
+  string temp = string(1, ch);
+  for(int i = 0; i < 8; i++) {
+    if(ch == operators[i]) {
+      showTokenLexeme.push_back(pair<string, string>("Operator: \t", temp));
+    }
+  }
+  for(int i = 8; i < 20; i++) {
+    if(ch == operators[i] && ch != '!' && !isspace(ch)) {
+      showTokenLexeme.push_back(pair<string, string>("Separator: \t", temp));
+    }
+  }
 }
 
 
